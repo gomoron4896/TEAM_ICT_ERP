a712f5e4e5817d1407f5ef4ad7d57a201fd01b1c
phw <phw4320@naver.com>
2018-10-26 17:14:57 +0900

    dd

--- src/main/java/com/ict/erp/controller/LoginController.java
+++ src/main/java/com/ict/erp/controller/LoginController.java
@@ -13,12 +13,12 @@ import com.ict.erp.service.UserInfoService;
 @Controller
 public class LoginController {
 
-	@Autowired
+	/*@Autowired
 	private UserInfoService uis;
 	
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public Integer putUser(HttpSession session, HttpServletResponse response) {
 		
 		return uis.putUserInfo(null);
-	}
+	}*/
 }
